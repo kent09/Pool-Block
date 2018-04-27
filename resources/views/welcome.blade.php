754ff7c20cd5035ca9ec4af224c04b6b7d2b81f0
Jeffrey Son <jeffxion03@gmail.com>
2018-04-27 15:07:16 +0800

    updates v2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="refresh" content="25">
+        
 
         <title>Pool Blocks</title>
 
